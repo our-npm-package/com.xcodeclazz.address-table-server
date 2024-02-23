fdbc6f94999f295ed3ad2e7d6930615d834b320c
Gaurav Gupta <gouravgupta840@gmail.com>
2024-02-23 12:51:21 +0530

    removed modals because mono will take care of that

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,4 @@
-export * from "./constants/users";
-
-export * from "./models/user";
 export * from "./models/render";
 
-export * from "./payloads/auth";
 export * from "./payloads/render";
-
-export * from "./responses/auth";
 export * from "./responses/render";
